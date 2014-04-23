7db6b54b7f5379b3bb3e0efafc72929a14731875
MathieuFromHome <mathie@matnard.net>
2014-04-23 01:48:50 -0400

    last update :shit:

--- scripts/Paddle.js
+++
@@ -1,9 +0,0 @@
-PONG.Paddle = function(){
-    PONG.Entity.apply(this, arguments);
-    this.width = 13;
-    this.height = 70;
-    this.graphics = [new PONG.Rect(0,0, this.width, this.height, this.color)];
-};
-
-PONG.Paddle.prototype = Object.create( PONG.Entity.prototype );
-PONG.Paddle.prototype.constructor = PONG.Paddle;
